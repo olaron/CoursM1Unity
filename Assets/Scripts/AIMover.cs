54bba4b2092723828db13014892dbef4e9fc62b3
Robin Alonzo <alonzo.robin7@gmail.com>
2021-11-25 09:38:01 +0100

    day2

--- Assets/Scripts/AIMover.cs
+++ Assets/Scripts/AIMover.cs
@@ -9,30 +9,43 @@ public class AIMover : MonoBehaviour
     private Animator anim;
     private static readonly int Speed = Animator.StringToHash("Speed");
 
+    [Tooltip("Max linear speed"), Range(0,15)]
+    public float maxSpeed = 5;
+    [Tooltip("Max angular speed"), Range(0,15)]
+    public float maxAngularSpeed = 5;
+
+    private Transform player;
+    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.inertiaTensor = Vector3.one;
+        rb.inertiaTensorRotation = new Quaternion(0, 0, 0,1);
         anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < -100)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     void FixedUpdate()
     {
         if (rb != null)
         {
-            if (Input.GetButton("Fire1"))
+            if (Input.GetButton("Fire1") && rb.velocity.magnitude < maxSpeed)
             {
-                rb.AddForce(transform.right * 100);    
+                rb.AddForce(transform.right * 30);    
             }
-            if (Input.GetButton("Fire2"))
+            if (Input.GetButton("Fire2") && rb.velocity.magnitude < maxAngularSpeed)
             {
-                rb.AddForce(transform.right * -100);    
+                rb.AddTorque(transform.up * 10);    
             }
         }
         
---
+++ Assets/Scripts/AISpawner.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AISpawner : MonoBehaviour
+{
+
+    public Transform prefabAI;
+    [Range(0,10)]
+    public float spawnInterval = 3;
+    private float time = 0;
+
+    public Transform spawnPoint;
+    private Vector3 lastSpawnForce;
+
+    private void Spawn()
+    {
+        Transform ai = Instantiate(prefabAI);
+        ai.position = spawnPoint.position;
+        ai.rotation = spawnPoint.rotation;
+        Rigidbody rb = ai.GetComponent<Rigidbody>();
+        Vector3 spawnForce = spawnPoint.right * 3;
+        Vector3 randomForce = new Vector3(Random.Range(-1f, 1f), Random.Range(0f, 1f), Random.Range(-1f, 1f));
+        randomForce = randomForce.normalized;
+        spawnForce += randomForce;
+        spawnForce *= 3;
+        rb.AddForce(spawnForce, ForceMode.Impulse);
+        lastSpawnForce = spawnForce;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > spawnInterval)
+        {
+            Spawn();
+            time -= spawnInterval;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(spawnPoint.position, spawnPoint.position + lastSpawnForce);
+    }
+}
