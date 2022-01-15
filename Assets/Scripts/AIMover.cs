5b4d05b8a76f363fbfc9097d12d91895853f652f
Robin Alonzo <alonzo.robin7@gmail.com>
2022-01-15 22:48:54 +0100

    Cleanup

--- Assets/Scripts/AIMover.cs
+++ Assets/Scripts/AIMover.cs
@@ -11,23 +11,7 @@ public class AIMover : MonoBehaviour
     private NavMeshAgent agent;
     private static readonly int Speed = Animator.StringToHash("Speed");
 
-    [Tooltip("Max linear speed"), Range(0,15)]
-    public float maxSpeed = 5;
-    [Tooltip("Max angular speed"), Range(0,15)]
-    public float maxAngularSpeed = 5;
-
-    private Transform destination;
-
-    private NavMeshPath path;
-    //private bool pathFound = false;
-
     private Transform player;
-    private Collider boxCollider;
-
-    private void Awake()
-    {
-        //destination = GameObject.FindGameObjectWithTag("Player").transform;
-    }
 
     // Start is called before the first frame update
     void Start()
@@ -38,14 +22,7 @@ public class AIMover : MonoBehaviour
         anim = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
         agent = GetComponent<NavMeshAgent>();
-        path = new NavMeshPath();
         agent.destination = player.position;
-        // pathFound = agent.CalculatePath(destination.position, path);
-        // if(!pathFound)
-        // {
-        //     Debug.LogError("Path not found");
-        // }
-        boxCollider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
@@ -59,21 +36,8 @@ public class AIMover : MonoBehaviour
         
     }
 
-    private void OnDrawGizmos()
-    {
-        // if (pathFound)
-        // {
-        //     Gizmos.color = Color.red;
-        //     for (int i = 0; i <= path.corners.Length - 2; i++)
-        //     {
-        //         Gizmos.DrawLine(path.corners[i],path.corners[i+1]);    
-        //     }
-        // }
-    }
-
     void FixedUpdate()
     {
-        //Debug.Log(agent.velocity.magnitude);
         anim.SetFloat(Speed, agent.velocity.magnitude);
     }
 
